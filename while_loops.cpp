fc8f53359b5edc35fb18a671e72b5d58075e4b4e
Taha Ansari <tahairfanansari@gmail.com>
2026-08-11 18:42:57 +0530

    Add while loop example in while_loops.cpp

---
+++ while_loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // WHILE LOOPS
+    int i,j;
+    cout<<"Enter Starting num: "<<endl;
+    cin>>i;
+    cout<<"Enter Ending num: "<<endl;
+    cin>>j;
+
+    while(i<=j){
+        cout<<i<<endl;
+        i++;
+    }
+return 0;
+}
