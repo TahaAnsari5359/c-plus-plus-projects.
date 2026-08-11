c8b73783faed6e6695cd537bb50d0dd28992933b
Taha Ansari <tahairfanansari@gmail.com>
2026-08-11 18:34:03 +0530

    Add basic for loop example in C++

---
+++ For_Loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // FOR LOOPS
+    cout<<"For Loop In C++"<<endl;
+    int i,j;
+    cout<<"Enter Starting Number: "<<endl;
+    cin>>i;
+    cout<<"Enter Ending Number: "<<endl;
+    cin>>j;
+    for (i; i<=j; i++){
+        cout<<"Num "<<i<<endl;
+    }
+
+    return 0;
+}
