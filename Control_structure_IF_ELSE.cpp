56e6b36878f50a61c0e7529f0f1f439e4e723e89
Taha Ansari <tahairfanansari@gmail.com>
2026-08-11 08:30:33 +0530

    Implement age-based voting eligibility check

---
+++ Control_structure_IF_ELSE.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Control Structure - IF, ELSE IF, ELSE"<<endl;
+    
+    cout<<"Enter Age: "<<endl;
+    cin>>age;
+    
+    if (age < 18 and age > 1){
+        cout<<"Sorry You Cannot Vote";
+        
+    }
+    else if (age < 1){
+        cout<<"InValid Age";
+        
+    }
+    else{
+        cout<<"You Can Vote"<<endl;
+    }
+  cout<<"Exiting Program..."<<endl;
+}
