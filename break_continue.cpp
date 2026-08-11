e4fbb73128a94c5b8c26b8b979638b448956a1ab
Taha Ansari <tahairfanansari@gmail.com>
2026-08-11 19:25:20 +0530

    Implement break and continue examples in C++
    Added examples of break and continue statements in a loop.

---
+++ break_continue.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Break and Continue Statements"<<endl;
+
+    for (int i = 1; i<=40; i++){
+        
+
+        //Break Statement
+        if (i == 10){
+            break;
+        }
+        cout<<i<<endl;
+    }
+    cout<<"Program Exited"<<endl;
+        }
+    }
+  //continue statement
+          if (i == 5) {
+            continue;
+        }
+
+        cout << i << endl;
+    }
+    cout<<"Program Skipped 5"<<endl;
+    
+
+return 0;
+}
