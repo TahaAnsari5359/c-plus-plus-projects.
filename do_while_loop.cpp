4111dfd8cd5f471ba337ba4ecb6c37e4cb14cebc
Taha Ansari <tahairfanansari@gmail.com>
2026-08-11 18:47:13 +0530

    Create do_while_loop.cpp

---
+++ do_while_loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // WHILE LOOPS
+    int i,j;
+    cout<<"Enter Starting num: "<<endl;
+    cin>>i;
+    cout<<"Enter Ending num: "<<endl;
+    cin>>j;
+
+    do{
+        cout<<i<<endl;
+        i++;
+    }
+    while(i<=j);
+return 0;
+}
