20e34d66764d80458e84a17bb94592e3dfecd760
Taha Ansari <tahairfanansari@gmail.com>
2026-08-18 12:42:47 +0530

    Add inline function for product calculation

---
+++ inline_func.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+inline int prod(int a, int b){
+  return a*b;
+    
+}
+
+
+int main()
+{
+    cout<<"inline Functions"<<endl;
+    cout<<"Product is: "<<prod(2,3);
+
+    return 0;
+}
