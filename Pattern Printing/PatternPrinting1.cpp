c4d3ecd3707d0920594e9b4b9a53becf79c612f2
Taha Ansari <tahairfanansari@gmail.com>
2026-09-17 13:05:07 +0530

    Add pattern printing functionality in C++
    Prints a pattern of numbers from 1 to n.

---
+++ Pattern Printing/PatternPrinting1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    cout<<"Pattern Printing"<<endl;
+
+    int n = 6;
+    for(int i = 1; i<=n; i ++){
+        for(int j = 1; j <=n; j ++){
+            cout<<j;    
+        }
+        cout<<endl;
+    }
+
+    
+
+    return 0;
+}
